b1ff976aec78a3cf25cd9b94496271365da2120d
Vikram Rayavarapu <46016457+rayavarapuvikram1@users.noreply.github.com>
2020-06-13 16:29:25 +0530

    mistake 5
    Forgot closing bracket

--- app/src/main/java/com/example/eggtimerapp/MainActivity.java
+++ app/src/main/java/com/example/eggtimerapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.eggtimerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -10,85 +8,103 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
     SeekBar timerSeekBar;
     TextView timerTextView;
     Button controllerButton;
-    Boolean counterIsActive=false;
+    Boolean counterIsActive = false;
     CountDownTimer countDownTimer;
 //    private Bundle savedInstanceState;
 
-    public void resetTimer(){
+    public void resetTimer()
+    {
         timerTextView.setText("0:30");
         timerSeekBar.setProgress(30);
         countDownTimer.cancel();
         controllerButton.setText("Go!");
         timerSeekBar.setEnabled(true);
-        counterIsActive=false;
+        counterIsActive = false;
     }
-    public void updateTimer(int secondsLeft){
-        int minutes=(int)secondsLeft/60;
-        int seconds=secondsLeft-minutes*60;
-        String secondString=Integer.toString(seconds);
-        if(seconds<=9){
-            secondString="0"+secondString;
-            timerTextView.setText(Integer.toString(minutes)+":"+secondString);
+
+    public void updateTimer(int secondsLeft)
+    {
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+        String secondString = Integer.toString(seconds);
+        if (seconds <= 9)
+        {
+            secondString = "0" + secondString;
+            timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
         }
-        timerTextView.setText(Integer.toString(minutes)+";"+Integer.toString(seconds));
+        timerTextView.setText(Integer.toString(minutes) + ";" + Integer.toString(seconds));
 
     }
-    public void controlTimer(View view){
-        counterIsActive=true;
-        if (counterIsActive==false){
+
+    public void controlTimer(View view)
+    {
+        counterIsActive = true;
+        if (counterIsActive == false)
+        {
             timerSeekBar.setEnabled(false);
             controllerButton.setText("stop");
 
-        countDownTimer= new CountDownTimer(timerSeekBar.getProgress()*1000+100,1000) {
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000)
+            {
 
-            @Override
-            public void onTick(long millisUntilfinished){
-                updateTimer((int)millisUntilfinished/1000);
-            }
-            @Override
-            public void onFinish(){
-                resetTimer();
-                MediaPlayer mplayer=MediaPlayer.create(getApplicationContext(),R.raw.alarm);
-                mplayer.start();
-            }
+                @Override
+                public void onTick(long millisUntilfinished)
+                {
+                    updateTimer((int) millisUntilfinished / 1000);
+                }
 
-        }.start();
+                @Override
+                public void onFinish()
+                {
+                    resetTimer();
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
+                    mplayer.start();
+                }
+
+            }.start();
 
 
-
-    }else{
+        }
+        else
+        {
             resetTimer();
         }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SeekBar timerSeekBar=(SeekBar)findViewById(R.id.timerSeekBar);
-        timerTextView=(TextView)findViewById(R.id.timerTextView);
-        controllerButton=(Button)findViewById(R.id.controllerButton);
+    }
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        controllerButton = (Button) findViewById(R.id.controllerButton);
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
         {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
                 updateTimer(progress);
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
 
             }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
 
             }
         });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    }
